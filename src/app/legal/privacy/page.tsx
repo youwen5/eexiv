5b7afa0c84ce789d9973f6a660c0ed54d1f91a84
Youwen Wu <youwenw@gmail.com>
2024-02-09 23:47:06 -0800

    fix build issue

--- src/app/legal/privacy/page.tsx
+++ src/app/legal/privacy/page.tsx
@@ -2,11 +2,11 @@ export default function Page() {
   return (
     <div className='content'>
       <p>
-        User privacy is important to us. Just kidding. We don't collect any
+        {`User privacy is important to us. Just kidding. We don't collect any
         personal information. We only use it to help us improve eeXiv. Your
         information may also be periodically sent to the Mossad to enhance
         profiling and cybersecurity operations. We take absolutely no
-        responsibility for misuse of your data.
+        responsibility for misuse of your data.`}
       </p>
     </div>
   )
