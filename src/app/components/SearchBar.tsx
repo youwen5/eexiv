12cc1ec29b58579a61f23bcc1a14539d4f085d5c
Youwen Wu <youwenw@gmail.com>
2024-02-14 16:51:00 -0800

    finally fix mobile styling

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -25,10 +25,10 @@ export default function SearchBar({
   }
 
   return (
-    <div className='w-full flex flex-nowrap'>
+    <div className='mx-2 flex flex-nowrap gap-2'>
       <input
         type='text'
-        className='py-3 px-5 rounded-xl text-slate-800 flex-grow'
+        className='py-3 px-5 rounded-xl text-slate-800 flex-grow flex-shrink min-w-0'
         name='q'
         placeholder='Search...'
         onChange={handleInputChange}
@@ -37,7 +37,7 @@ export default function SearchBar({
       />
       <button
         type='submit'
-        className='p-2.5 mx-4 border-2 rounded-xl hover:bg-blue-300 flex-shrink'
+        className='p-2.5 border-2 rounded-xl hover:bg-blue-300 flex-shrink'
         onClick={handleClick}
       >
         Search
