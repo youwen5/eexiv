5707214092e154f56bf489c747871a4be86e76a0
Youwen Wu <youwenw@gmail.com>
2024-02-15 14:05:06 -0800

    remove comment

--- src/app/document/view/[slug]/page.tsx
+++ src/app/document/view/[slug]/page.tsx
@@ -2,11 +2,6 @@
 import DocumentViewer from './DocumentViewer'
 import ErrorBoundary from '@/app/utils/ErrorBoundary'
 
-// export function generateStaticParams() {
-//   const docsList = Object.keys(documents)
-//   return docsList.map((doc) => ({ doc }))
-// }
-
 export default function Page({
   params,
 }: Readonly<{ params: { slug: string } }>) {
