38cd4392c6ddbf8af717893257f55a37a32078ce
Ananth Venkatesh <46249765+quantum9Innovation@users.noreply.github.com>
2024-02-16 22:44:04 +0000

    Fix Sonarlint errors on new WIP affiliations page

--- src/app/affiliation/page.tsx
+++ src/app/affiliation/page.tsx
@@ -52,7 +52,7 @@ const Page = () => {
       </h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {allAffiliations.map((affiliationShortName) => {
-          const { name, short, image } = affiliations[affiliationShortName]
+          const { name, short } = affiliations[affiliationShortName]
           return (
             <Fragment key={name}>
               <AffiliationCard
