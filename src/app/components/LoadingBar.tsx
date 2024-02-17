7152dfb051cc3316bcc8205f6f9eb5009d856208
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2024-02-17 12:54:07 -0800

    style: update LoadingBar component background color to blue

--- src/app/components/LoadingBar.tsx
+++ src/app/components/LoadingBar.tsx
@@ -2,7 +2,7 @@ import styles from './loading.module.css'
 const LoadingBar = () => {
   return (
     <div className='w-full fixed top-0 left-0'>
-      <div className='h-1.5 w-full bg-pink-100 overflow-hidden'>
+      <div className='h-1.5 w-full bg-blue-100 overflow-hidden'>
         <div
           className={`${styles.progress} w-full h-full bg-blue-500 ${styles['left-right']}`}
         ></div>
