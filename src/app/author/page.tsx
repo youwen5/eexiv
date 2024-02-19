aed6bf88f50f1ab3079354ba7fbd4e1cafc34b9c
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2024-02-18 21:23:24 -0800

    fix: update heading text from "Affiliations" to "Authors"

--- src/app/author/page.tsx
+++ src/app/author/page.tsx
@@ -57,7 +57,7 @@ const Page = () => {
   return (
     <div className='p-6'>
       <h1 className={`${zillaSlab.className} text-6xl text-center mb-10`}>
-        Affiliations
+        Authors
       </h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {Object.keys(authors).map((authorShortName) => {
