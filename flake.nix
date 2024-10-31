fbb2d23e9732194874b0fc91a4a2524b49ca432f
Youwen Wu <youwenw@gmail.com>
2024-10-31 16:05:13 -0700

    dev: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs =
+    inputs@{ flake-parts, devenv-root, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          devenv.shells.default = {
+            devenv.root =
+              let
+                devenvRootFileContent = builtins.readFile devenv-root.outPath;
+              in
+              pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+
+            name = "eeXiv";
+
+            imports = [
+              # This is just like the imports in devenv.nix.
+              # See https://devenv.sh/guides/using-with-flake-parts/#import-a-devenv-module
+              # ./devenv-foo.nix
+            ];
+
+            # https://devenv.sh/reference/options/
+            packages =
+              [ config.packages.default ]
+              ++ (with pkgs; [
+                prettierd
+                tailwindcss-language-server
+              ]);
+
+            processes = {
+              dev.exec = "npm run dev";
+            };
+
+            languages = {
+              typescript.enable = true;
+              javascript = {
+                enable = true;
+                npm.enable = true;
+                npm.install.enable = true;
+              };
+            };
+          };
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
