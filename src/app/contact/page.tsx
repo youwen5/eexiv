2e63b173d5a212ffc1f22bda446af751a8d4b5fb
Youwen Wu <youwenw@gmail.com>
2025-04-30 00:30:54 -0700

    feat: update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,11 +3,10 @@ export default function Page() {
     <div className='content text-slate-800'>
       <h1 className='text-3xl text-slate-800 mt-4 mb-2 font-serif'>Contact</h1>
       <p>
-        You can contact a team representative directly at{' '}
-        <a href='mailto:sanramonvalleyrobotics@gmail.com'>
-          sanramonvalleyrobotics@gmail.com
-        </a>
-        . If you encounter any problems with the site or eeXiv content,{' '}
+        As eeXiv is no longer under the stewardship of Team 1280, you should
+        contact the current{' '}
+        <a href='https://web.youwen.dev/impressum'>webmaster</a> if any issues
+        arise . If you encounter any problems with the site or eeXiv content,{' '}
         <a href='https://github.com/youwen5/eeXiv/issues'>open an issue</a> and
         provide a helpful description of the problem. For ways you can help, see{' '}
         <a href='../help'>Help</a>.
