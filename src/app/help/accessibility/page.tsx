3924a4e517247e7adbfb747988bed2b96c2a98b4
Youwen Wu <youwenw@gmail.com>
2025-04-30 00:28:03 -0700

    feat: update accessibility page

--- src/app/help/accessibility/page.tsx
+++ src/app/help/accessibility/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
       <h1 className='text-3xl text-slate-800 mt-4 mb-2 font-serif'>
         Accessibility
       </h1>
-      <p>
+      <p className='line-through'>
         If you encounter any accessibility-related issues related to your use of
         our site, it is likely because of our jank code architecture.
         Unfortunately, our programming team is ill equipped to fix these issues,
@@ -13,6 +13,16 @@ export default function Page() {
         submitting a{' '}
         <a href='https://github.com/youwen5/eeXiv/pull/new'>pull request</a> on
         GitHub.
+      </p>
+      <p>
+        Unfortunately due to budget cuts and in order to comply with <a
+        href="https://www.whitehouse.gov/presidential-actions/2025/01/ending-radical-and-wasteful-government-dei-programs-and-preferencing/">executive
+          orders</a> issued by the current administration, we are can no longer
+        accomodate any Diversity, Equity, Includion, and Accessibility (DEIA)
+        concerns. Our jank code architecture is now an integral component in
+        "unleashing American innovation and excellence" by ensuring anyone with
+        impairments or low fidelity hardware who struggle to access our site
+        continue to struggle.
       </p>
       <h2 className='text-2xl text-slate-800 mt-6 mb-1 font-serif'>
         Supported Hardware
@@ -23,10 +33,17 @@ export default function Page() {
         little RAM. This is because we load the entire database of documents,
         authors, topics, affiliations, and other data/metadata directly into
         memory via JavaScript. As a result, the site may be slow or unusable on
-        low-spec devices and it can only get worse. If you would like to remedy
-        this issue, we again recommend you open a{' '}
-        <a href='https://github.com/youwen5/eeXiv/pull/new'>pull request</a> and
-        port our in memory database to an actual remote database.
+        low-spec devices and it can only get worse.{' '}
+        <span className='line-through'>
+          If you would like to remedy this issue, we again recommend you open a{' '}
+          <a href='https://github.com/youwen5/eeXiv/pull/new'>pull request</a>{' '}
+          and port our in memory database to an actual remote database.
+        </span>
+      </p>
+      <p>
+        Again, we reiterate that this is now an intended feature. If your
+        hardware cannot handle our awesome architecture, you should purchase a
+        new American-made computer powered by American chips.
       </p>
     </div>
   )
